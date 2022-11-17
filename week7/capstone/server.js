1d749ab352028ba5bb2992f63d8f1da81f309102
Nickthef <nicolas.franks@bryanuniversity.edu>
2022-11-17 16:52:36 -0500

    capstone

---
+++ week7/capstone/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const movieRouter = require('./routes/movieRouter');
+
+const app = express();
+const PORT = 9000;
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// Routes
+app.use('/movies', movieRouter);
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.log(err);
+    return res.send({errMsg: err.message})
+});
+
+// Server startup logic
+app.listen(PORT, () => {
+    console.log(`App started on port: ${PORT}`);
+});
